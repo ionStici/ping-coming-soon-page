7393e1a1a2656ecad457ec0b77961fc5c61e24b1
Ion Stici <ionsticidev@gmail.com>
2023-01-07 11:07:36 +0200

    Add Readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ping Coming Soon Page
+
+This is a solution to the [Ping coming soon page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ping-single-column-coming-soon-page-5cadd051fec04111f7b848da). The goal is to create a simple and responsive "Coming Soon" page that includes an email subscription form to notify users about the launch.
+
+| [Live Demo Link](https://ionstici.github.io/ping-coming-soon-page) | [Frontend Mentor](https://www.frontendmentor.io/solutions/ping-single-column-coming-soon-page-ZdLZ42ke1S) |
+| ------------------------------------------------------------------ | --------------------------------------------------------------------------------------------------------- |
+
+![](./images/screenshot.png)
--- script.js
+++ script.js
@@ -5,61 +5,53 @@ const message = document.querySelector(".form__message");
 const input = document.querySelector(".form__email-input");
 const btn = document.querySelector(".btn");
 
-//
-
 const mediaQuery = window.matchMedia("(min-width: 767px)");
 
 const checkViewport = function (viewport) {
-    if (viewport.matches) {
-        btn.style.top = "0";
-    } else {
-        btn.style.top = "3.2rem";
-    }
+  if (viewport.matches) {
+    btn.style.top = "0";
+  } else {
+    btn.style.top = "3.2rem";
+  }
 };
 
 window.addEventListener("resize", function (event) {
-    if (event.target.innerWidth <= 767 && message.style.opacity === "1") {
-        // 767
-        btn.style.top = "3.2rem";
-    } else if (
-        event.target.innerWidth >= 767 &&
-        message.style.opacity === "1"
-    ) {
-        btn.style.top = "0";
-    }
+  if (event.target.innerWidth <= 767 && message.style.opacity === "1") {
+    // 767
+    btn.style.top = "3.2rem";
+  } else if (event.target.innerWidth >= 767 && message.style.opacity === "1") {
+    btn.style.top = "0";
+  }
 });
 
-//
-
 function checkIfEmailIsValid(input) {
-    var validRegex =
-        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    if (input.match(validRegex)) {
-        // Valid
-        return true;
-    } else {
-        // Invalid
-        return false;
-    }
+  if (input.match(validRegex)) {
+    // Valid
+    return true;
+  } else {
+    // Invalid
+    return false;
+  }
 }
 
 const formController = function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (!checkIfEmailIsValid(input.value)) {
-        message.style.opacity = "1";
-        input.style.border = "1px solid #ff5466";
-        checkViewport(mediaQuery);
-    } else {
-        input.value = "";
-        document.querySelector(".p").textContent = "Thank you!";
-        document.querySelector(".p").style.color = "#ff5466";
+  if (!checkIfEmailIsValid(input.value)) {
+    message.style.opacity = "1";
+    input.style.border = "1px solid #ff5466";
+    checkViewport(mediaQuery);
+  } else {
+    input.value = "";
+    document.querySelector(".p").textContent = "Thank you!";
+    document.querySelector(".p").style.color = "#ff5466";
 
-        btn.style.top = "0";
-        message.style.opacity = "0";
-        input.style.border = "1px solid #c2d3ff";
-    }
+    btn.style.top = "0";
+    message.style.opacity = "0";
+    input.style.border = "1px solid #c2d3ff";
+  }
 };
 
 btn.addEventListener("click", formController);
